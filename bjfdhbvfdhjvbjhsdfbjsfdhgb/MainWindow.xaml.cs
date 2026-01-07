a76dae8a4e8f4993103edcf8d3e9fb7524729291
Petrik Diák <diak@petrik.hu>
2026-01-07 09:26:37 +0100

    esés + ugrás + háttér

--- bjfdhbvfdhjvbjhsdfbjsfdhgb/MainWindow.xaml.cs
+++ bjfdhbvfdhjvbjhsdfbjsfdhgb/MainWindow.xaml.cs
@@ -8,17 +8,37 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace bjfdhbvfdhjvbjhsdfbjsfdhgb
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        private DispatcherTimer dispatcherTimer;
+
+		public MainWindow()
         {
             InitializeComponent();
-        }
-    }
+
+			dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+			dispatcherTimer.Tick += new EventHandler(Update);
+			dispatcherTimer.Interval = TimeSpan.FromMilliseconds(1);
+			dispatcherTimer.Start();
+
+			this.KeyDown += new KeyEventHandler(Ugras);
+		}
+
+		private void Update(object sender, EventArgs e)
+		{
+			madar.SetValue(Canvas.TopProperty, Canvas.GetTop(madar) + 3);
+		}
+
+		private void Ugras(object sender, KeyEventArgs e)
+		{
+			if (e.Key == Key.Space)
+			{
+				madar.SetValue(Canvas.TopProperty, Canvas.GetTop(madar) - 60);
+			}
+		}
+	}
 }
