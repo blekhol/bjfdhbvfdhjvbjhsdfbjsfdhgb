cc88ff520b09ce751edc65d29a86f0d1c3cd6197
Petrik Diák <diak@petrik.hu>
2026-01-14 10:35:10 +0100

    oszlop mozgás + oszlop class

--- bjfdhbvfdhjvbjhsdfbjsfdhgb/MainWindow.xaml.cs
+++ bjfdhbvfdhjvbjhsdfbjsfdhgb/MainWindow.xaml.cs
@@ -15,9 +15,11 @@ namespace bjfdhbvfdhjvbjhsdfbjsfdhgb
 {
     public partial class MainWindow : Window
     {
-        private DispatcherTimer dispatcherTimer;
+        DispatcherTimer dispatcherTimer;
 
-		public static Random rnd = new Random();
+		Random rnd = new Random();
+
+		List<Oszlop> oszlopok = [];
 
 		public MainWindow()
         {
@@ -25,15 +27,27 @@ namespace bjfdhbvfdhjvbjhsdfbjsfdhgb
 
 			dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
 			dispatcherTimer.Tick += new EventHandler(Update);
-			dispatcherTimer.Interval = TimeSpan.FromMilliseconds(1);
+			dispatcherTimer.Interval = TimeSpan.FromMilliseconds(8);
 			dispatcherTimer.Start();
 
-			this.KeyDown += new KeyEventHandler(Ugras);
+			this.KeyUp += new KeyEventHandler(Ugras);
+
+			for (int i = 0; i < 9; i += 2)
+			{
+				UjOszlop();
+				oszlopok[i].OszlopBox.SetValue(Canvas.LeftProperty, Canvas.GetLeft(oszlopok[0].OszlopBox) + 100 * i);
+				oszlopok[i+1].OszlopBox.SetValue(Canvas.LeftProperty, Canvas.GetLeft(oszlopok[0].OszlopBox) + 100 * i);
+			}
 		}
 
 		private void Update(object sender, EventArgs e)
 		{
 			madar.SetValue(Canvas.TopProperty, Canvas.GetTop(madar) + 1.5);
+
+			foreach (var oszlop in oszlopok)
+			{
+				oszlop.OszlopBox.SetValue(Canvas.LeftProperty, Canvas.GetLeft(oszlop.OszlopBox) - 1.5);
+			}
 		}
 
 		private void Ugras(object sender, KeyEventArgs e)
@@ -42,6 +56,30 @@ namespace bjfdhbvfdhjvbjhsdfbjsfdhgb
 			{
 				madar.SetValue(Canvas.TopProperty, Canvas.GetTop(madar) - 40);
 			}
+		}
+
+		private void UjOszlop()
+		{
+			int felsoOszlopTop = rnd.Next(-240, -64);
+			int alsoOszlopTop = felsoOszlopTop + 440;
+
+			Oszlop felsoOszlop = new Oszlop();
+			Oszlop alsoOszlop = new Oszlop();
+
+			felsoOszlop.OszlopLetrehozas(false);
+			alsoOszlop.OszlopLetrehozas(true);
+
+			felsoOszlop.OszlopBox.SetValue(Canvas.LeftProperty, (double)700);
+			alsoOszlop.OszlopBox.SetValue(Canvas.LeftProperty, (double)700);
+
+			felsoOszlop.OszlopBox.SetValue(Canvas.TopProperty, (double)felsoOszlopTop);
+			alsoOszlop.OszlopBox.SetValue(Canvas.TopProperty, (double)alsoOszlopTop);
+
+			canvas.Children.Add(felsoOszlop.OszlopBox);
+			canvas.Children.Add(alsoOszlop.OszlopBox);
+
+			oszlopok.Add(felsoOszlop);
+			oszlopok.Add(alsoOszlop);
 		}
 	}
 }
--- bjfdhbvfdhjvbjhsdfbjsfdhgb/Oszlop.cs
+++ bjfdhbvfdhjvbjhsdfbjsfdhgb/Oszlop.cs
@@ -11,14 +11,15 @@ namespace bjfdhbvfdhjvbjhsdfbjsfdhgb
 {
     class Oszlop
     {
-        private GroupBox oszlopBox;
+		private GroupBox oszlopBox = new GroupBox();
 
-        public Oszlop()
-        {
-            this.oszlopBox = new GroupBox();
+		public Oszlop()
+		{
 		}
 
-		public void OszlopLetrehozas()
+		public GroupBox OszlopBox { get => oszlopBox; }
+
+		public void OszlopLetrehozas(bool forditott)
         {
             StackPanel stackPanel = new StackPanel();
 
@@ -26,6 +27,27 @@ namespace bjfdhbvfdhjvbjhsdfbjsfdhgb
 			felsoRect.Width = 75;
             felsoRect.Height = 30;
             felsoRect.Stroke = Brushes.Black;
+            felsoRect.Fill = Brushes.Green;
+
+			System.Windows.Shapes.Rectangle alsoRect = new System.Windows.Shapes.Rectangle();
+			alsoRect.Width = 60;
+			alsoRect.Height = 280;
+			alsoRect.Stroke = Brushes.Black;
+			alsoRect.Fill = Brushes.Green;
+
+			if (forditott)
+            {
+				stackPanel.Children.Add(felsoRect);
+				stackPanel.Children.Add(alsoRect);
+			}
+			else
+			{
+				stackPanel.Children.Add(alsoRect);
+				stackPanel.Children.Add(felsoRect);
+			}
+
+			oszlopBox.Content = stackPanel;
+            oszlopBox.BorderThickness = new System.Windows.Thickness(0);
 		}
 	}
 }
